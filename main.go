53529d113a9e3201fefffd3a6aef71ea233c3041
Sam <sam.smucker@gmail.com>
2024-02-03 14:12:15 -0600

    add basic web server

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 func connectionHandle() *sql.DB {
@@ -43,4 +45,10 @@ func connectionHandle() *sql.DB {
 
 func main() {
 	connectionHandle()
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
