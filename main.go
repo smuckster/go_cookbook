a435e6a60e9b7140dc6491a57986aaf6736c29d8
Sam <sam.smucker@gmail.com>
2024-02-05 19:56:05 -0600

    example simple web server serving static files and compiled templates

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -10,14 +12,23 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func connectionHandle() *sql.DB {
 	var err error
 
 	err = godotenv.Load(".env")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 
 	cfg := mysql.Config{
@@ -44,12 +55,29 @@ func connectionHandle() *sql.DB {
 	return db
 }
 
+func handleIndex(c echo.Context) error {
+	return c.Render(http.StatusOK, "index", struct{}{})
+}
+
 func main() {
 	connectionHandle()
 
+	// Register templates
+	t := &Template{
+		templates: template.Must(template.ParseGlob("index.html")),
+	}
+
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
-	})
+	e.Use(
+		middleware.Recover(),
+		middleware.Logger(),
+	)
+	e.HTTPErrorHandler = func(err error, c echo.Context) {
+		fmt.Println(c.Path(), c.QueryParams(), err.Error())
+		e.DefaultHTTPErrorHandler(err, c)
+	}
+	e.Static("/static", "assets")
+	e.Renderer = t
+	e.GET("/", handleIndex)
 	e.Logger.Fatal(e.Start(":8080"))
 }
